3a59cc1ddad1e06a440180e7d8bb0e0201887d5b
KostyaBobroff <k.bobrov.jr@gmail.com>
2018-09-14 23:44:27 +0300

    Add enum Currency, protocol Bank, class Account

--- DZ1/DZ1/main.swift
+++ DZ1/DZ1/main.swift
@@ -8,5 +8,49 @@
 
 import Foundation
 
-print("Hello, World!")
+protocol BankProtocol {
+    func printInfo()
+}
 
+enum Currency: String {
+    case RUR = "RUR"
+    case USD = "USD"
+    case EUD = "EUD"
+    
+}
+
+class Account : BankProtocol {
+    var id: Int
+    var balance: Double
+    var currency: Currency?
+    var  description: String
+    var offer: String?
+
+    init(id: Int, balance: Double, currency: String, description: String, offer: String? = nil ) {
+        self.id = id
+        self.balance = balance
+        self.currency = Currency(rawValue:currency)
+        self.description = description
+        self.offer = offer
+    }
+    func printInfo(){
+        print("User account \nuser id: \(id) \nbalance: \(balance) \ncurrency: \(currency!.rawValue) \noffer: \(offer ?? "offers don't exist")")
+    }
+}
+
+var a = Account(id:1, balance: 12.3, currency: "RURs", description:"My first User")
+a.printInfo()
+
+
+//
+//class ReissueInfo{
+//
+//}
+//
+//class Card {
+//
+//}
+//
+//class ServerResponce {
+//
+//}
