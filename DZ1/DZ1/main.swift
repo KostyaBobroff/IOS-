c69150ba0327f309962df7be25a1e23d52f8df9e
KostyaBobroff <k.bobrov.jr@gmail.com>
2018-09-14 20:25:47 +0300

    First commit

---
+++ DZ1/DZ1/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  DZ1
+//
+//  Created by Costa Bobroff on 14.09.2018.
+//  Copyright © 2018 Costa Bobroff. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
